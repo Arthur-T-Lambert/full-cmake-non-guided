8008f8fb28a85be9b97234cc9cd064255ce71ffa
Arthur Lambert <arthurlambert95@gmail.com>
2024-06-20 15:12:08 +0200

    Add prime feature

---
+++ include/prime.h
@@ -0,0 +1,13 @@
+#ifndef PRIME_H
+#define PRIME_H
+
+/**
+ * @file prime.h
+ * @brief Check if a number is a prime number.
+ *
+ * @param a Integer.
+ * @return Bool True if the number is a prime number, False otherwise.
+ */
+int prime(int a);
+
+#endif // PRIME_H
---
+++ src/prime.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file prime.cpp
+ * @brief Implementation of prime function.
+ */
+
+#include "prime.h"
+
+int prime(int a) {
+    return false;
+}
