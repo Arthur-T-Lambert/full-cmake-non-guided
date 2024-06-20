05bef609af6c5c4acc84b5df9b38e2cdea41322d
Arthur Lambert <arthurlambert95@gmail.com>
2024-06-20 14:31:01 +0200

    Add main function

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @brief Main function : demonstration of the features.
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Feature demonstration " << std::endl;
+
+    return 0;
+}
